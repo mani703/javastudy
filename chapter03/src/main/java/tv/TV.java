442239126405b35f844b852d2e2b05d33a8c1c78
BIT <BIT@DESKTOP-MPCH5KV>
2021-04-28 16:48:34 +0900

    solved TV class pratice06

--- chapter03/src/main/java/tv/TV.java
+++ chapter03/src/main/java/tv/TV.java
@@ -5,27 +5,71 @@ public class TV {
 	private int volume;		// 0 ~ 100
 	private boolean power;
 	
-	public void power(boolean on) {
+	public TV() {
 		
 	}
 	
-	public void channel(boolean up) {
-		
+	public TV(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+	}
+	
+	public int getChannel() {
+		return channel;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public boolean isPower() {
+		return power;
+	}
+
+	public void power(boolean on) {
+		if(on) {
+			power = on;
+		} else {
+			power = false;
+			channel = 0;
+			volume = 0;
+		}
+	}
+	
+	public void channel(boolean up) {	
+		channel++;
+		if(channel > 255) {
+			channel = 255;
+		}
 	}
 	
 	public void channel(int channel) {
-		
+		if(channel < 1 || channel > 255) {
+			this.channel = 1;
+		} else {
+			this.channel = channel;
+		}
 	}
 	
 	public void volume(boolean up) {
-		
+		volume++;
+		if(volume > 100) {
+			volume = 100;
+		}
 	}
 	
-	public void volume(int channel) {
-		
+	public void volume(int volume) {
+		if(volume < 0) {
+			this.volume = 0;
+		} else if(volume > 100) {
+			this.volume = 100;
+		} else {
+			this.volume = volume;
+		}
 	}
 	
 	public void status() {
-		System.out.println("TV[power=on, channel=11, volume=30");
+		System.out.println("TV[power= " + power + ", channel= " + channel + ", volume= " + volume);
 	}
 }
