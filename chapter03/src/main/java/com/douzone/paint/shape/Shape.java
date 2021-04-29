28ca527789e717390ce85a76d8d144a96c142f5b
BIT <BIT@DESKTOP-MPCH5KV>
2021-04-29 11:47:40 +0900

    packaging

--- chapter03/src/main/java/com/douzone/paint/shape/Shape.java
+++ chapter03/src/main/java/com/douzone/paint/shape/Shape.java
@@ -1,4 +1,6 @@
-package paint;
+package com.douzone.paint.shape;
+
+import com.douzone.paint.i.Drawable;
 
 public abstract class Shape implements Drawable{	// 추상적인 클래스 new를 하지 않기 위해
 	private String lineColor;
