dfaf88576badd09e9fb15cc1c7f4cf3920948f21
BIT <BIT@DESKTOP-MPCH5KV>
2021-04-30 09:51:30 +0900

    String Test #1

--- chapter04/src/main/java/chapter04/ObjectTest02.java
+++ chapter04/src/main/java/chapter04/ObjectTest02.java
@@ -14,7 +14,6 @@ public class ObjectTest02 {
 //		.equals() : 두 객체의 동질성(내용비교)
 //					Object의 기본 구현은 동일성(==) 비교와 같다.
 		System.out.println(p1.equals(p2));
-		System.out.println(p2.equals(p3));
+		System.out.println(p2.equals(p3));	
 	}
-
 }
---
+++ chapter04/src/main/java/chapter04/StringTest01.java
@@ -0,0 +1,22 @@
+package chapter04;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+//		c:\temp
+		System.out.println("c:\\temp");
+		
+//		"hello
+		System.out.println("\"hello\"");
+		
+//		\t : tab
+//		\r : carriage return - 맨 앞으로 이동
+//		\n : new line
+//		\b : beep
+		
+//		'
+		char c = '\'';
+		System.out.println(c);
+	}
+
+}
