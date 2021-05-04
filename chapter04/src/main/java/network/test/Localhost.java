014b28cbeaa04e900ac3bd4d44f2c666e70cdc20
BIT <BIT@DESKTOP-MPCH5KV>
2021-05-04 11:55:02 +0900

    socket server

---
+++ chapter04/src/main/java/network/test/Localhost.java
@@ -0,0 +1,30 @@
+package network.test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Localhost {
+
+	public static void main(String[] args) {
+		// localhost 127.0.0.1 
+		try {
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			
+			String hostName = inetAddress.getHostName();
+			String hostAddress = inetAddress.getHostAddress();
+			
+			System.out.println(hostName);
+			System.out.println(hostAddress);
+			
+			byte[] addresses = inetAddress.getAddress();
+			for(byte address : addresses) {
+//				System.out.print(address);
+				System.out.print(address & 0x000000ff);
+				System.out.print(".");
+			}
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+}
