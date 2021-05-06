8730fd365115cc07190b3a47892177ab4824a6be
BIT <BIT@DESKTOP-MPCH5KV>
2021-05-06 12:30:12 +0900

    httpd 작성 #2

--- network/src/main/java/httpd/RequestHandler.java
+++ network/src/main/java/httpd/RequestHandler.java
@@ -1,13 +1,16 @@
 package httpd;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.file.Files;
 
 public class RequestHandler extends Thread {
+	private static final String DOCUMENTROOT = "./webapp";
 	private Socket socket;
 	
 	public RequestHandler( Socket socket ) {
@@ -18,7 +21,7 @@ public class RequestHandler extends Thread {
 	public void run() {
 		try {
 			// get IOStream
-			OutputStream outputStream = socket.getOutputStream();
+			OutputStream os = socket.getOutputStream();
 			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
 
 			// logging Remote Host IP Address & Port
@@ -47,15 +50,29 @@ public class RequestHandler extends Thread {
 				}
 			}
 
-			consoleLog(request);
-
+			// 요청 처리
+			String[] tokens = request.split(" ");
+			if("GET".equals(tokens[0])) {
+				consoleLog("request: " + tokens[1]);
+				responseStaticResource(os, tokens[1], tokens[2]);
+			} else {	// methods : POST, PUT, DELETE, HEAD, CONNECT
+				/* Simple Http Server 에서는 무시 */
+				
+				// 응답 예시
+				// HTTP/1.1 400 Bad Request\r\n;
+				// Content-Type:text/html; charset=utf-8\r\n
+				// \r\n
+				// HTML 에러 문서 (./webapp/error/400.html)
+				
+				// response400Error(os, tokens[1], tokens[2]);
+			}
 			
 			// 예제 응답입니다.
 			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
-			outputStream.write( "HTTP/1.1 200 OK\r\n".getBytes( "UTF-8" ) );
-			outputStream.write( "Content-Type:text/html; charset=utf-8\r\n".getBytes( "UTF-8" ) );
-			outputStream.write( "\r\n".getBytes() );
-			outputStream.write( "<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes( "UTF-8" ) );
+			// os.write( "HTTP/1.1 200 OK\r\n".getBytes( "UTF-8" ) );
+			// os.write( "Content-Type:text/html; charset=utf-8\r\n".getBytes( "UTF-8" ) );
+			// os.write( "\r\n".getBytes() );
+			// os.write( "<h1>이 페이지가 잘 보이면 실습과제 SimpleHttpServer를 시작할 준비가 된 것입니다.</h1>".getBytes( "UTF-8" ) );
 
 		} catch( Exception ex ) {
 			consoleLog( "error:" + ex );
@@ -70,6 +87,34 @@ public class RequestHandler extends Thread {
 				consoleLog( "error:" + ex );
 			}
 		}			
+	}
+
+	private void responseStaticResource(OutputStream os, String url, String protocol) throws IOException{
+		// welcom file set
+		if("/".equals(url)) {
+			url = "/index.html";
+		}
+		
+		File file = new File(DOCUMENTROOT + url);
+		if(file.exists() == false) {
+			// 응답 예시
+			// HTTP/1.1 404 File Not Found OK\r\n;
+			// Content-Type:text/html; charset=utf-8\r\n
+			// \r\n
+			// HTML 에러 문서 (./webapp/error/404.html)
+			
+			// response404Error(os, url, protocol);
+			return;
+		}
+		
+		// new io (nio)
+		byte[] body = Files.readAllBytes(file.toPath());
+		String contentType = Files.probeContentType(file.toPath());
+		os.write((protocol + " 200 OK\r\n").getBytes("UTF-8"));
+		os.write(("Content-Type:" + contentType + "; charset=utf-8\r\n").getBytes( "UTF-8" ));
+		os.write("\r\n".getBytes());
+		os.write(body);
+
 	}
 
 	public void consoleLog( String message ) {
